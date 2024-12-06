3c6d4d6ba9e1f140124a08dd24adad9cb970223d
Abdulrahman Waseem <132848451+awaseem1987@users.noreply.github.com>
2024-12-06 21:08:25 +0200

    Add files via upload

---
+++ data/partners.js
@@ -0,0 +1,420 @@
+const egpartners = [
+  {
+    id: "",
+    order: null,
+    name: "Techniconsult",
+    logo: "logos/techniconsult.png",
+    website: "https://www.tcfirenze.com/en/",
+    devision: "engineering"
+  },
+  {
+    id: "",
+    order: null,
+    name: "PARTECO Modular Cleanroom System",
+    logo: "https://parteco.it/wp-content/uploads/2021/01/parteco-1024x256.png",
+    website: "https://parteco.it/en/",
+    devision: "facility"
+  },
+  {
+    id: "",
+    order: null,
+    name: "STERILINE",
+    logo: "https://www.steriline.it/i/logo.png",
+    website: "https://www.steriline.it/en/home/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "ZIRBUS",
+    logo: "	https://www.zirbus.com/wp-content/themes/zirbus/dist/images/logo_9202bb09.svg",
+    website: "https://www.zirbus.com/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "TM ITALIA",
+    logo: "logos/tm-italia.png",
+    website: "https://www.tm-italia.it/index.php?lin=inglese",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "PLUEMAT",
+    logo: "logos/Plumat.jpg",
+    website: "https://www.pluemat.info/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "SOLME",
+    logo: "https://www.solme.com/wp-content/uploads/logo_solme.png",
+    website: "https://www.solme.com/en/",
+    devision: "preparation"
+  },
+  {
+    id: "",
+    order: null,
+    name: "FREUND",
+    logo: "https://www.freund.co.jp/common/images/logo.svg",
+    website: "https://www.freund.co.jp/english/",
+    //https://www.cosmec-it.com/en
+    devision: "non-sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "KIKUSUI",
+    logo: "logos/kikusui.png",
+    website: "http://www.kikusui.com/en/Home",
+    devision: "non-sterile"
+  },
+
+  {
+    id: "",
+    order: null,
+    name: "OMAS",
+    logo: "logos/omas.png",
+    website: "https://www.omastecnosistemi.it/",
+    devision: "non-sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "OMAG",
+    logo: "logos/omag.png",
+    website: "https://www.omag-pack.com/en",
+    devision: "non-sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "FAMAR TEC",
+    logo: "logos/famarTec.png",
+    website: "https://www.famartecsrl.com/",
+    devision: "non-sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "PRISMA",
+    logo: "logos/prisma.png",
+    website: "https://www.prismaindustriale.com/en-US/home-page.aspx",
+    devision: "secondary-packaging"
+  },
+  {
+    id: "",
+    order: null,
+    name: "ARCA Labeling & Marking",
+    logo: "logos/arca-removebg-preview.png",
+    website: "https://www.arcalabelingmarking.com/en/",
+    devision: "secondary-packaging"
+  },
+  {
+    id: "",
+    order: null,
+    name: "PIERI",
+    logo: "https://www.pieri.it/wp-content/uploads/2018/08/logo-pieri.png",
+    website: "https://www.pieri.it/en/",
+    devision: "secondary-packaging"
+  },
+
+  // {
+  //   id: "",
+  //   order: null,
+  //   name: "METAPACK",
+  //   logo: "logos/metapack.png",
+  //   website: "https://www.metapack.it/",
+  //   devision: "sterile"
+  // },
+  // {
+  //   id: "",
+  //   order: null,
+  //   name: "STI Engineering",
+  //   logo: "logos/sti-engineering.jpg",
+  //   website: "https://www.sti-engineering.it/en/",
+  //   devision: "engineering"
+  // },
+  {
+    id: "",
+    order: null,
+    name: "MPA ITALY",
+    logo: "../logos/mpa.png",
+    website: "https://mpaitaly.com/en/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "TEMA SINERGIE",
+    logo: "logos/TEMAsinergie.png",
+    website: "https://www.temasinergie.com/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "ABC TRANSFER",
+    logo: "logos/abcTransfer.png",
+    website: "https://abctransfer.fr/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "RIGEL LIFE SCIENCES",
+    logo: "logos/rigel.png",
+    website: "https://www.rigellifesciences.com/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "ELETTRACQUA",
+    logo: "logos/elettracqua.png",
+    website: "http://www.elettracqua.com/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "GENERAL FILTERS",
+    logo: "https://www.generalfilter.com/wp-content/uploads/2024/01/logo-general-filter-1.png",
+    website: "https://hfiltration.it/en/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "CDZ",
+    logo: "logos/cdz.png",
+    website: "https://www.cdzsrl.net/",
+    devision: "electro-mechanical"
+  },
+  {
+    id: "",
+    order: null,
+    name: "HFILTRATION",
+    logo: "logos/hfiltration.png",
+    website: "https://hfiltration.it/en/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "IPM Italia",
+    logo: "logos/ipm.png",
+    website: "https://www.ipmitalia.it/en/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "INEXPORT LIGHT",
+    logo: "logos/inexport-light.png",
+    website: "https://www.inexportlight.com/it/",
+    devision: "sterile"
+  }
+
+
+  // {
+  //   id: "",
+  //   order: null,
+  //   name: "BISIO PROGETTI",
+  //   logo: "logos/bisio-progetti.png",
+  //   website: "http://www.bisioprogetti.it/it/index.html",
+  //   devision: "sterile"
+  // }
+];
+
+
+
+
+
+
+const ksapartners = [
+  {
+    id: "",
+    order: null,
+    name: "Techniconsult",
+    logo: "logos/techniconsult.png",
+    website: "https://www.tcfirenze.com/en/",
+    devision: "engineering"
+  },
+  {
+    id: "",
+    order: null,
+    name: "DELTA 2000 Clean Rooms",
+    logo: "logos/delta2000.jpg",
+    website: "https://parteco.it/en/",
+    devision: "facility"
+  },
+  {
+    id: "",
+    order: null,
+    name: "STERILINE",
+    logo: "https://www.steriline.it/i/logo.png",
+    website: "https://www.steriline.it/en/home/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "ZIRBUS",
+    logo: "	https://www.zirbus.com/wp-content/themes/zirbus/dist/images/logo_9202bb09.svg",
+    website: "https://www.zirbus.com/",
+    devision: "sterile"
+  },
+  
+  {
+    id: "",
+    order: null,
+    name: "TM ITALIA",
+    logo: "logos/tm-italia.png",
+    website: "https://www.tm-italia.it/index.php?lin=inglese",
+    devision: "sterile"
+  },
+
+  {
+    id: "",
+    order: null,
+    name: "SOLME",
+    logo: "https://www.solme.com/wp-content/uploads/logo_solme.png",
+    website: "https://www.solme.com/en/",
+    devision: "preparation"
+  },
+
+
+  {
+    id: "",
+    order: null,
+    name: "OMAS",
+    logo: "logos/omas.png",
+    website: "https://www.omastecnosistemi.it/",
+    devision: "non-sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "OMAG",
+    logo: "logos/omag.png",
+    website: "https://www.omag-pack.com/en",
+    devision: "non-sterile"
+  },
+
+  {
+    id: "",
+    order: null,
+    name: "FAMAR TEC",
+    logo: "logos/famarTec.png",
+    website: "https://www.famartecsrl.com/",
+    devision: "non-sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "PRISMA",
+    logo: "logos/prisma.png",
+    website: "https://www.prismaindustriale.com/en-US/home-page.aspx",
+    devision: "secondary-packaging"
+  },
+  {
+    id: "",
+    order: null,
+    name: "MPA ITALY",
+    logo: "../logos/mpa.png",
+    website: "https://mpaitaly.com/en/",
+    devision: "sterile"
+  },
+
+
+
+  {
+    id: "",
+    order: null,
+    name: "ARCA Labeling & Marking",
+    logo: "logos/arca-removebg-preview.png",
+    website: "https://www.arcalabelingmarking.com/en/",
+    devision: "secondary-packaging"
+  },
+
+
+  {
+    id: "",
+    order: null,
+    name: "ABC TRANSFER",
+    logo: "logos/abcTransfer.png",
+    website: "https://abctransfer.fr/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "RIGEL LIFE SCIENCES",
+    logo: "logos/rigel.png",
+    website: "https://www.rigellifesciences.com/",
+    devision: "sterile"
+  },
+
+
+
+
+
+  {
+    id: "",
+    order: null,
+    name: "ELETTRACQUA",
+    logo: "logos/elettracqua.png",
+    website: "http://www.elettracqua.com/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "GENERAL FILTERS",
+    logo: "https://www.generalfilter.com/wp-content/uploads/2024/01/logo-general-filter-1.png",
+    website: "https://hfiltration.it/en/",
+    devision: "electro-mechanical"
+  },
+  {
+    id: "",
+    order: null,
+    name: "CDZ",
+    logo: "logos/cdz.png",
+    website: "https://www.cdzsrl.net/",
+    devision: "electro-mechanical"
+  },
+  {
+    id: "",
+    order: null,
+    name: "HFILTRATION",
+    logo: "logos/hfiltration.png",
+    website: "https://hfiltration.it/en/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "IPM Italia",
+    logo: "logos/ipm.png",
+    website: "https://www.ipmitalia.it/en/",
+    devision: "sterile"
+  },
+  {
+    id: "",
+    order: null,
+    name: "INEXPORT LIGHT",
+    logo: "logos/inexport-light.png",
+    website: "https://www.inexportlight.com/it/",
+    devision: "sterile"
+  },
+
+];
+
+
+
+
+
